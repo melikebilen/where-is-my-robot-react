27400afd2bca476f0f85f12f466f7f0395597a9b
melikebilen <bilenmelike@gmail.com>
2021-05-09 14:05:58 +0300

    Initial commit for React Website

---
+++ .history/src/App_20210503220655.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import './App.css';
+import {database} from './firebase';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data : null
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount(){
+    database.ref().on('value', (snapshot) => {
+        this.setState({
+          data: snapshot.val(),
+          newData: '',
+          count: snapshot.child('Count/count').val(),
+          x: snapshot.child('RobotLocation/x').val(),
+          y: snapshot.child('RobotLocation/y').val(),
+        });
+    });
+  }
+  handleChange(e){
+    const newData = e.target.value;
+    this.setState({
+      newData: newData
+    });
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    database.ref().child('AMAZINGNEWDATA').set(this.state.newData); //we can use push instead of set here , database.ref() is the root!
+    //database.ref("/AMAZINGNEWDATA").push(this.state.newData);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App--header">
+          <h2>Welcome to React and Firebase</h2>
+          <h2> Count : {JSON.stringify(this.state.count, null,2)}</h2>
+          <h2> X : {JSON.stringify(this.state.x, null,2)}</h2>
+          <h2> Y : {JSON.stringify(this.state.y, null,2)}</h2>
+        </div>
+        <pre className="App--data">
+          {JSON.stringify(this.state.data, null,2)}
+        </pre>
+        <form className="App--form" onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.newData}  onChange={this.handleChange}/>
+          <input type="submit"/>        
+        </form>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ .history/src/firebase_20210503214856.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBQjxxJqCcYhjYDH6GFwjSGdQ7VeqVGhCI",
+    authDomain: "tdoa-23cf7.firebaseapp.com",
+    databaseURL: "https://tdoa-23cf7-default-rtdb.firebaseio.com",
+    projectId: "tdoa-23cf7",
+    storageBucket: "tdoa-23cf7.appspot.com",
+    messagingSenderId: "611153024504",
+    appId: "1:611153024504:web:0f5e82fbf77d611aaf720d",
+    measurementId: "G-YYER7FY9DQ"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
+
+export const database = firebase.database();
